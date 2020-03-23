4ba514349d49d84370a785d0fb5d2dd5b446e81d
evan <aspaasevan@gmail.com>
2020-03-23 15:01:57 -0700

    routes working for / and /movies/id

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-const MovieList = props => {
+const MovieList = (props) => {
   const [movies, setMovies] = useState([])
+  
   useEffect(() => {
+    
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
@@ -21,7 +24,9 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
